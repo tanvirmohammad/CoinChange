bd7a2bf04ee9d0d2682bf55f3f97318f92bb4070
tanvirmohammad <tanvirmohammad2@gmail.com>
2018-04-06 01:56:37 +0600

    Update Dynamic.java

--- Dynamic.java
+++ Dynamic.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class CoinChange_Dynamic {
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter Coin Size: ");
+        System.out.print("Enter Coin Length: ");
         int x = in.nextInt();
         int coin[] = new int[x];
-        System.out.println("Inser Coins: ");
-        for (int i = 0; i < x; i++) {
+        System.out.print("Enter Coins: ");
+        for (int i=0;i<x;i++){
             coin[i] = in.nextInt();
         }
-        System.out.println("Enter Amount: ");
+        System.out.print("Enter Amount: ");
         int amount = in.nextInt();
         Coin(coin,amount);
     }
@@ -18,22 +18,22 @@ public class CoinChange_Dynamic {
         int row = coin.length;
         int col = amount+1;
         int table[][] = new int[row][col];
-        for(int i=0;i<row;i++){
+        for (int i=0;i<row;i++){
             table[i][0] = 0;
         }
-        for(int i=1;i<col;i++){
+        for (int i=1;i<col;i++){
             table[0][i] = i/coin[0];
         }
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
+        for (int i=1;i<row;i++){
+            for (int j=1;j<col;j++){
                 if(coin[i]>j){
-                    table[i][j]=table[i-1][j];
+                    table[i][j] = table[i-1][j];
                 }else{
-                    table[i][j]=Math.min(table[i-1][j], table[i][j-coin[i]]+1);
+                    table[i][j] = Math.min(table[i-1][j],table[i][j-coin[i]]+1);
                 }
             }
         }
-// Uncomment for show the table.
+//       Uncomment for show the table.
 
 //        for(int i=0;i<row;i++){
 //            for (int j=0;j<col;j++){
@@ -41,28 +41,26 @@ public class CoinChange_Dynamic {
 //            }
 //            System.out.println();
 //        }
-        int i=row-1,j=col-1;
-        int count = 0;
-        System.out.println("Total Coin Needed "+table[row-1][col-1]);
-        System.out.print("Coin Used : ");
+
+        System.out.println("Total Coin Needed: "+table[row-1][col-1]);
+        System.out.print("Coin Used: ");
+        int i=row-1,j=col-1,count=0;
         while(i>0 && j>0){
             if(table[i][j] == table[i-1][j]){
                 i--;
                 continue;
             }
             System.out.print(coin[i]+" ");
-            j=j-coin[i];
+            j -= coin[i];
             count++;
         }
-        while(count!=table[row-1][col-1] && j>=coin[0]){
+        while (count != table[row-1][col-1] && j>=coin[0]){
             System.out.print(coin[0]+" ");
-            j=j-coin[0];
+            j -= coin[0];
             count++;
         }
         if(j!=0){
             System.out.println("Problem");
         }
-
     }
-
 }
